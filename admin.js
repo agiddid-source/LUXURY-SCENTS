5c1713b450dd7b4c216088e43a5f314d014037a7
agiddid-source <agiddid@gmail.com>
2026-04-20 10:27:06 +0100

    commit

--- admin.js
+++ admin.js
@@ -8,12 +8,43 @@ function addProduct() {
     return;
   }
 
-  const product = { name, price, image };
 
   let products = JSON.parse(localStorage.getItem("products")) || [];
-  products.push(product);
+  products.push({ name, price, image });
 
   localStorage.setItem("products", JSON.stringify(products));
 
-  alert("Product added!");
+  document.getElementById("productName").value = "";
+  document.getElementById("productPrice").value = "";
+  document.getElementById("productImage").value = "";
+
+  alert("Product added successfully!");
+  renderAdminProducts();
 }
+
+function deleteProduct(index) {
+  let products = JSON.parse(localStorage.getItem("products")) || [];
+  products.splice(index, 1);
+  localStorage.setItem("products", JSON.stringify(products));
+  renderAdminProducts();
+}
+
+function renderAdminProducts() {
+  const container = document.getElementById("adminProducts");
+  const products = JSON.parse(localStorage.getItem("products")) || [];
+
+  container.innerHTML = "";
+
+  products.forEach((p, index) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <p>${p.name} - ₦${p.price}</p>
+    <button onclick="deleteProduct(${index})">Delete</button>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+renderAdminProducts();
